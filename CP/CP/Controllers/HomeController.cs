88afba5bc95a64e2deb435e30088ea456778580a
LiftenShy <privalovsergey95@gmail.com>
2016-06-14 00:16:31 +0300

    Add function upload and show image users

--- CP/CP/Controllers/HomeController.cs
+++ CP/CP/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using CP.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +16,18 @@ namespace CP.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult IndexPost(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                string pic = Path.GetFileName(file.FileName);
+                string path = Path.Combine(Server.MapPath("~/Images/"), pic);
+                file.SaveAs(path);
+                this.ViewData["Image"] = "~/Images/" + pic;
+            }
+            return this.View("Index");
         }
     }
 }
