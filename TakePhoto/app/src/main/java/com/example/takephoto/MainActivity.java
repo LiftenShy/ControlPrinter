e71f787df9062aed78d341790503ab345a835652
LiftenShy <privalovsergey95@gmail.com>
2016-06-17 10:11:37 +0300

    Back move Android project

---
+++ TakePhoto/app/src/main/java/com/example/takephoto/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.takephoto;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends Activity {
+
+    private static final int CAM_REQUEST = 1313;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAM_REQUEST) {
+            //"http://controlprinter.apphb.com/Home/IndexPost"
+
+            final File f = new File(getFilesDir(),"pic5.jpg");
+            //Convert bitmap to byte array
+            ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+            Bitmap bmp = (Bitmap) data.getExtras().get("data");
+            bmp.compress(Bitmap.CompressFormat.JPEG, 0, byteStream);
+            byte[] bitmapdata = byteStream.toByteArray();
+
+            //write the bytes in file
+            FileOutputStream fos = null;
+            try {
+                f.createNewFile();
+                fos = new FileOutputStream(f);
+                fos.write(bitmapdata);
+                fos.flush();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://controlprinter.apphb.com/Home/IndexPost");
+
+            try {
+                // Add your data
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+                nameValuePairs.add(new BasicNameValuePair("name", "Hello"));
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+            }
+        }
+    }
+
+    public void TakePhoto_Click(View v) throws UnknownHostException {
+        Intent cameraintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraintent, CAM_REQUEST);
+    }
+}
